551a6249d192e1855dbb45eeffc22dac79355723
JulienBianchin <jurienu@gmail.com>
2024-11-12 22:49:02 +0100

    Add animation CSS library and title

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ load_dotenv()
 headers = [Link(rel="icon", href="/public/image/favicon.ico", type="image/x-icon")]
 
 if "prod" == os.getenv("ENV"):
+    headers.append(Link(rel="stylesheet", href="/public/css/animate.min.css"))
     headers.append(Link(rel="stylesheet", href="/public/css/style.min.css"))
 else:
     headers.append(Script(src='https://cdn.tailwindcss.com'))
+    headers.append(Link(rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"))
 
 app, rt     = fast_app(live=True, hdrs=headers)
 supabase    = create_client(
@@ -57,19 +59,20 @@ def card_players(players):
 def get():
     return Div(
         Div(
-            Ul(
-                Li("New game"),
-                Li("Continue"),
-                Li("Options"),
-                A(
-                    Li("Players"),
-                    href='/players',
-                    cls='hover:text-blue-500'
-                ),
-                Li("Credits"),
-                cls='font-extrabold text-orange-50 text-2xl text-right italic mx-16'
+            Div(
+                H1("Alchemixa", cls='font-extrabold text-orange-400 text-6xl italic'),
+                Img(src='/public/image/favicon.ico'),
+                cls='mx-16 flex items-center animate__animated animate__pulse animate__infinite animate__slower'
             ),
-            cls='w-screen h-screen bg-orange-700/50 grid grid-cols-1 content-end pb-16'
+            Div(
+                A("New game",   href='#'),
+                A("Continue",   href='#'),
+                A("Options",    href='#'),
+                A("Players",    href='/players', cls='hover:text-blue-500'),
+                A("Credits",    href='#'),
+                cls='flex flex-col font-extrabold text-orange-50 text-xl italic text-right mx-16'
+            ),
+            cls='w-screen h-screen bg-orange-700/60 grid justify-items-end content-end pb-16 gap-8'
         ),
         cls='w-screen h-screen bg-no-repeat bg-center bg-cover bg-[url("/public/image/home.jpeg")]'
     )
